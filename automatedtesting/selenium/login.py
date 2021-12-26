b17334fab4f4145c929a2aa2b69b7be9422fe3d0
Marko-Buda <44259192+Marko-Buda@users.noreply.github.com>
2021-12-26 19:08:01 +0100

    Update login.py
    Add --disable-dev-shm-usage argument

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -25,6 +25,7 @@ def login (user, password):
     options = ChromeOptions()
     options.add_argument("--no-sandbox")
     options.add_argument("--headless")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
     #driver = webdriver.Chrome('/usr/local/bin/chromedriver')
 
